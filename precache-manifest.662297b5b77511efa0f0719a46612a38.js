83035c6ad9be60bdb986c0947af4f84f3dba5db5
Russell Wakugawa <wakugawa@usc.edu>
2020-06-13 00:49:53 -0700

    Updates

---
+++ precache-manifest.662297b5b77511efa0f0719a46612a38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e272ccb4ade1b9975acff140aa463f4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f346550642a5b49d2d92",
+    "url": "/static/css/main.af3d5214.chunk.css"
+  },
+  {
+    "revision": "556d6342da7e8caa6c89",
+    "url": "/static/js/2.09952e45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.09952e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f346550642a5b49d2d92",
+    "url": "/static/js/main.cee554c2.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  },
+  {
+    "revision": "cd1d6d67c331b59dcd8294326a2f3a1b",
+    "url": "/static/media/bali.cd1d6d67.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aa54c42bdacdf08c157294b57fe1c24c",
+    "url": "/static/media/landscape_goober.aa54c42b.jpg"
+  }
+]);
