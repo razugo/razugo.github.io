d3e1d8e4552a083d2298a8fb7aa83e52d01a4f41
Russell Wakugawa <rwakugawa@gmail.com>
2020-12-29 14:46:35 -0800

    updating resume colors

--- src/Components/js/ResumeTile.js
+++ src/Components/js/ResumeTile.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import moduleClasses from '../css/ResumeTile.module.css';
+
 function resumetile(props) {
     
     var time = props.time;
@@ -10,9 +12,9 @@ function resumetile(props) {
     return (
         <div class="col-md-6">
             <div className="bg-white border rounded p-4 mb-4">
-                <p className="badge badge-primary text-2 font-weight-400">{ time }</p>
+                <p className={ moduleClasses.resumeTime }>{ time }</p>
                 <h3 className="text-5">{ position }</h3>
-                <p className="text-danger">{ company }</p>
+                <p className={ moduleClasses.resumeCompany }>{ company }</p>
                 <p className="mb-0">{ description }</p>
             </div>
         </div>
